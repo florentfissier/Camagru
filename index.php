d3846c8f7721967b6ae6ecfd6942c3a1f6df7a7d
florent fissier <florent.fissier@gmail.com>
2017-12-06 14:59:15 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My awesome landing page</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	Voici le body
+</body>
+</html>
